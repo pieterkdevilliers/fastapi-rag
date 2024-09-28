ad58dc55aa6d17e9daa3d1ac2598b1025d581656
Pieter K de Villiers <pieter@hey.com>
2024-09-28 08:06:16 +0100

    Ensure Upload Files and SourceFile model respects account_unique_id

--- file_management/utils.py
+++ file_management/utils.py
@@ -2,11 +2,11 @@ from sqlmodel import Session
 from file_management.models import SourceFile
         
 
-def save_file_to_db(filename: str, file_path: str, session: Session):
+def save_file_to_db(filename: str, file_path: str, file_account: str, session: Session):
     """
     Save Source File to DB
     """
-    db_file = SourceFile(file_name=filename, file_path=file_path)
+    db_file = SourceFile(file_name=filename, file_path=file_path, account_unique_id=file_account)
     session.add(db_file)
     session.commit()
     session.refresh(db_file)
