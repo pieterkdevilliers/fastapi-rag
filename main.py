3de0a986737927b383974652ea4f3612da1153ca
Pieter K de Villiers <pieter@hey.com>
2025-09-20 13:35:10 +0100

    bfa-814 sort variable error

--- main.py
+++ main.py
@@ -487,7 +487,7 @@ async def add_score_card_result(request: Request, session: Session = Depends(get
             print("Processing: Quiz Finished")
             # Access nested data correctly
             result_id = nested_data.get("result_id")
-            score_card_result = await int_utils.create_or_update_score_card_result(result_id, result_id, account_unique_id, session)
+            score_card_result = await int_utils.create_or_update_score_card_result(result_id, nested_data, account_unique_id, session)
             print("score_card_result: ", score_card_result)
             
         elif event_name == "LEAD_DETAILS_UPDATED":
