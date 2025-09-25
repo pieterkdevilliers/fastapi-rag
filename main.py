961759d7c2679363c561115316094c1b7932c1c5
Pieter K de Villiers <pieter@hey.com>
2025-09-25 12:56:01 +0100

    Tie test query to logged-in user

--- main.py
+++ main.py
@@ -688,7 +688,7 @@ async def process_internal_widget_query(
 ):
     account_unique_id = current_user["account_unique_id"]
     query = payload.query.strip() if payload.query else None
-    visitor_email = payload.email if payload.email else None
+    visitor_email = current_user.user_email
     print('******Payload: ', payload)
 
     if not query:
