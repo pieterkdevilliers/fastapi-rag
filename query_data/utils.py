eb531f719f8f2517858db049efef3589aa4ebf25
Pieter K de Villiers <pieter@hey.com>
2025-09-30 16:36:29 +0100

    bfa-876 debug response

--- query_data/utils.py
+++ query_data/utils.py
@@ -23,6 +23,7 @@ async def call_repo_b(query_payload: Query):
     print('Query being sent: ', query_payload)
     print('Query being sent model_dump: ', query_payload.model_dump())
     print('Headers: ', headers)
+    print('Repo B URL: ', REPO_B_URL)
     async with httpx.AsyncClient(timeout=60.0) as client:
         try:
             
