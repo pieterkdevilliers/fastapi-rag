d2fa6c1c7aa7ea2615709912554b7bfe958edeab
Pieter K de Villiers <pieter@hey.com>
2025-10-02 16:37:36 +0100

    Amend True comparrison

--- accounts/utils.py
+++ accounts/utils.py
@@ -124,7 +124,7 @@ def get_notification_users(account_unique_id: str, session: Session):
     """
     Get Users who should receive notifications
     """
-    statement = select(User).filter(User.account_unique_id == account_unique_id)
+    statement = select(User).filter(User.account_unique_id == account_unique_id, User.receive_notifications == True)
     result = session.exec(statement)
     users = result.all()
     print('Notification Users Found: ', users)
