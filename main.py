ccbbea5210e359d8829f4529b8fdc47f3c637de7
Pieter K de Villiers <pieter@hey.com>
2026-01-26 14:36:45 +0000

    Add colourmap

--- main.py
+++ main.py
@@ -2888,6 +2888,7 @@ async def generate_wordcloud(account_unique_id: str,
         background_color='white', 
         max_words=200,
         collocations=False,
+        colormap='viridis',
         ).generate_from_frequencies(wordcloud_data)
 
     # Save Wordcloud Image
