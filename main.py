c0dbcc5b1593edc25b64fa45f6308cfa972bd219
Pieter K de Villiers <pieter@hey.com>
2026-01-28 12:21:27 +0000

    Update print statements

--- main.py
+++ main.py
@@ -1020,7 +1020,7 @@ async def process_internal_widget_query_agent(
         if len(chat_history) > 1:
             print("Not analyzing sentiment for non-initial queries.")
             sentiment = await query_utils.update_conversation_sentiment(query_payload=agent_payload)
-            print("Chat History: ", chat_history_dicts)
+            print(f"Updated sentiment for query '{query}': {sentiment}")
     
     return StreamingResponse(
         generate(),
